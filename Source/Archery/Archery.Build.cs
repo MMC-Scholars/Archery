28c43efbe94dc9a59350c1374775b91b81141646
MMC-Scholars <mmcscholarsvr@gmail.com>
2019-09-06 14:56:52 -0400

    Fixed VRBase relative reference in build.cs - Sam

--- Source/Archery/Archery.Build.cs
+++ Source/Archery/Archery.Build.cs
@@ -1,22 +1,13 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+using System.IO;
 using UnrealBuildTool;
 
-public class Archery : ModuleRules
-{
-	public Archery(ReadOnlyTargetRules Target) : base(Target)
-	{
+public class Archery : ModuleRules {
+	public Archery(ReadOnlyTargetRules Target) : base(Target) {
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate", "SlateCore" });
         PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay", "SteamVR" });
-
-        //These allow us to reference files in the VRBase directory from anywhere in the project - you may need to add
-        //another path for your machine.
-        PublicIncludePaths.Add("C:/Users/Public/Public Projects/Archery/Source/Archery/VRBase");
-        PublicIncludePaths.Add("D:/Github/Archery/Source/Archery/VRBase");
-        PublicIncludePaths.Add("C:/Users/Public/Documents/Public Projects/Archery/Source/Archery/VRBase");
-
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        
+        PublicIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "VRBase")));
     }
 }
