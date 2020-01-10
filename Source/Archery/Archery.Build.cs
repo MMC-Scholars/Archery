5364a54613c78090fb83790556c9207bb564931c
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-01-10 17:10:36 -0500

    Update with VRBase 2.0 - Sam

--- Source/Archery/Archery.Build.cs
+++ Source/Archery/Archery.Build.cs
@@ -1,13 +1,15 @@
 using System.IO;
 using UnrealBuildTool;
 
-public class Archery : ModuleRules {
-	public Archery(ReadOnlyTargetRules Target) : base(Target) {
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+public class Archery : ModuleRules
+{
+    public Archery(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate", "SlateCore" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate", "SlateCore", "ProceduralMeshComponent" });
         PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay", "SteamVR" });
-        
+
         PublicIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "VRBase")));
     }
 }
